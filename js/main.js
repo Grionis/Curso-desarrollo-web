d216af8581348015c502a1103204178ccf006d7f
Grionis <rasoriumabyssus@gmail.com>
2018-03-12 12:41:54 -0300

    new script

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 /**  */
 (function(){
     'use strict';
+    var regalo = document.getElementById('regalo');
+
     document.addEventListener('DOMContentLoaded', function(){
       
 
@@ -12,14 +14,39 @@
 
         //Campos pases
         var pase_dia = document.getElementById('pase_dia');
-        var pase_dosdia = document.getElementById('pase_dosdisas');
+        var pase_dosdia = document.getElementById('pase_dos_dias');
         var pase_completo = document.getElementById('pase_completo');
 
         //botones y divs
         var calcular = document.getElementById('calcular');
         var errorDiv = document.getElementById('error');
         var botonRegistro = document.getElementById('btnRegistro');
-        var resultado = document.getElementByid('lista-productos');
+        var resultado = document.getElementById('lista-productos');
         
+
+        //extras
+
+        var camisas = document.getElementById('camisa_evento');
+        var etiquetas = document.getElementById('etiquetas');
+
+        calcular.addEventListener('click', calcularMontos);
+
+        function calcularMontos(event){
+            event.preventDefault();
+            if(regalo.value === '')
+            {
+                alert('debe seleccionar un regalo');
+                regalo.focus();
+            }
+            else
+            {
+                var boletoDia = pase_dia.value,
+                    boleto2Dia = pase_dosdia.value,
+                    boletoCompleto = pase_completo.value;
+
+                    var total = (boletoDia * 30) + (boleto2Dia * 30) + (boleto2Dia * 50);
+                    console.log('total a pagar:' + total );
+            }
+        }
     });// DOM CONTECT LOADED
 })();
