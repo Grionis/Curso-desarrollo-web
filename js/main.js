d5f2f1f2ef1b4e9562f082a5086a5e59ebfad408
Grionis <rasoriumabyssus@gmail.com>
2018-03-13 18:49:51 -0300

    js: add map to page index.html

--- js/main.js
+++ js/main.js
@@ -1,4 +1,34 @@
 /**  */
+var apiMap = 'AIzaSyAryKBaK6uc9TcMHD4GFDPPyVaCHC_gMkg';
+
+function initMap() {
+   
+    var latLng = {
+        lat : -33.432748,
+        lng : -70.6223707
+    }; 
+
+    var map = new google.maps.Map(document.getElementById('mapa'), {
+      'center': latLng,
+      zoom: 14
+    });
+
+    var informacion = new google.maps.InfoWindow({
+        content: 'hola mundo'
+    });
+
+    var marker = new google.maps.Marker({
+        position: latLng,
+        map: map,
+        tittle: 'Duoc educacion continua'
+    });
+
+    marker.addListener('click', function(){
+        informacion.open(map,marker)
+    });
+}
+
+
 (function(){
     'use strict';
     var regalo = document.getElementById('regalo');
@@ -30,7 +60,28 @@
         var etiquetas = document.getElementById('etiquetas');
 
         calcular.addEventListener('click', calcularMontos);
+        pase_dia.addEventListener('blur',mostrar);
+        pase_dosdia.addEventListener('blur', mostrar);
+        pase_completo.addEventListener('blur', mostrar);
 
+
+        nombre.addEventListener('blur', validarCampo);
+        apellido.addEventListener('blur', validarCampo);
+        email.addEventListener('blur',validarCampo);
+        function validarCampo()
+        {
+            if(this.value == '')
+            {
+                errorDiv.style.display = 'block';
+                errorDiv.innerHTML = 'este campo es obligatoio';
+                this.style.border = '1px solid red';
+                errorDiv.style.border = '1px solid red';
+            }
+            else{
+                errorDiv.style.display = 'none';
+                this.style.border = ' 1px solid #cccccc';
+            }
+        }
         function calcularMontos(event){
             event.preventDefault();
             if(regalo.value === '')
@@ -60,7 +111,7 @@
                 
                      listado_productos.innerHTML = '';
                      suma.innerHTML = '';
-
+                     listado_productos.style.display = "block";
                     for(var i = 0; i < listaProductos.length; i ++)
                     {
                         listado_productos.innerHTML  += listaProductos[i] + '<br/>';
@@ -69,6 +120,24 @@
 
                     suma.innerHTML = "$ " + parseInt(total, 10) ||0;
 
+            }
+        }
+
+        function mostrar()
+        {
+            var boletoDia = parseInt( pase_dia.value, 10) || 0,
+                    boleto2Dia = parseInt(pase_dosdia.value, 10) || 0,
+                    boletoCompleto = parseInt(pase_completo.value, 10) || 0 ;
+
+            var diasElegidos = [];
+            
+            if(boletoDia > 0){ diasElegidos.push('viernes');}
+            if(boleto2Dia > 0) {diasElegidos.push('viernes', 'sabado');}
+            if(boletoCompleto > 0) {diasElegidos.push('viernes','sabado','domingo');}
+
+            for(var i = 0; i < diasElegidos.length; i++)
+            {
+                document.getElementById(diasElegidos[i]).style.display = 'block';
             }
         }
     });// DOM CONTECT LOADED
